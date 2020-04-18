bd4e55f1264a75f70439fc7b8b4975927cedd110
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2020-04-18 11:49:13 +0200

    - Added multiple channels with multiple videos - Fetched channel data using channel username as well as a list of recent uploads

--- vueTask/pages/index.vue
+++ vueTask/pages/index.vue
@@ -1,65 +1,68 @@
 <template>
   <div class="container">
-    <!-- // TODO: add one channel with some of its videos (channelSections)-->
-    <nuxt-link v-bind:to="temp">Video</nuxt-link>
+    <div class="list" v-for="(channelData, i) in channelVideosData" :key="i">
+      <ul><b>{{Object.keys(channelData)[0]}}</b></ul>
+      <li v-for="(video, j) in channelData[Object.keys(channelData)[0]]" :key="j">
+        <nuxt-link :to="'/video/'+video.videoId">{{video.videoTitle}}</nuxt-link>
+      </li>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios'
 
 export default {
-  components: {
-  },
-  data () {
+  components: {},
+  data() {
     return {
-      channels: [],
-      channelUserNames:["triplejtv", "nprmusic", "RiotGamesInc"],
-      temp: "/videos"
+      channelUserNames: [
+        'nprmusic',
+        'childishgambinoch',
+        'kanyewest',
+        'triplejtv',
+        'PowerfulJRE'
+      ],
+      channelVideosData: []
     }
   },
-  async fetch () {
-    let channel = await axios.get(`https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails&forUsername=triplejtv&key=${process.env.API_KEY}`)
-    .catch((err)=>console.log(err));
-    console.log(channel);
+  async fetch() {
+    // ? refactor
+    this.channelUserNames.forEach(async channelUsername => {
+      let channel = await axios
+        .get(
+          `https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails&forUsername=${channelUsername}&key=${process.env.API_KEY}`
+        )
+        .catch(err => console.log(err))
+      console.log(channel)
+      let recentUploadsPlaylistId =
+        channel.data.items[0].contentDetails.relatedPlaylists.uploads
+      let channelRecentUploads = await axios
+        .get(
+          `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=5&playlistId=${recentUploadsPlaylistId}&key=${process.env.API_KEY}`
+        )
+        .catch(err => console.log(err))
 
-    let recentUploadsPlaylistId = channel.data.items[0].contentDetails.relatedPlaylists.uploads;
-
-    let channelRecentUploads = await axios.get(`https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=5&playlistId=${recentUploadsPlaylistId}&key=${process.env.API_KEY}`)
-    .catch((err)=>console.log(err));
-    console.log(channelRecentUploads);
-    
-    this.temp = "/video/"+channelRecentUploads.data.items[0].contentDetails.videoId
+      let channelTitle = channelRecentUploads.data.items[0].snippet.channelTitle
+      this.channelVideosData.push({
+        [channelTitle]: channelRecentUploads.data.items.map(
+          item => {return {videoId: item.contentDetails.videoId, videoTitle: item.snippet.title}}
+        )
+      })
+    })
   }
 }
 </script>
 
 <style scoped>
 .container {
-  margin: 0 auto;
+  margin: 2rem auto;
   min-height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+  justify-content: space-between;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  /* text-align: center; */
 }
 
 .links {
--- vueTask/pages/video/index.vue
+++ vueTask/pages/video/index.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="container">
-      Default Video Page
-    <iframe
-      id="player"
-      type="text/html"
-      width="640"
-      height="390"
-      src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
-      frameborder="0"
-    ></iframe>
+      You Shouldn't Be Here o_o
   </div>
 </template>
 
@@ -29,25 +21,4 @@ export default {
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
