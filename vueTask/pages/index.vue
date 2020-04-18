3147637639264111a2841746711787f466cb3e90
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2020-04-18 14:22:00 +0200

    - Added a couple of comments for readability

--- vueTask/pages/index.vue
+++ vueTask/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <!-- Creating a simple ul for each channel and its recent videos -->
     <div class="list" v-for="(channelData, i) in channelVideosData" :key="i">
       <ul><b>{{Object.keys(channelData)[0]}}</b></ul>
       <li v-for="(video, j) in channelData[Object.keys(channelData)[0]]" :key="j">
@@ -29,12 +30,14 @@ export default {
   async fetch() {
     // ? refactor
     this.channelUserNames.forEach(async channelUsername => {
+      // Fetches a channel's data including its recent uploads playlist
       let channel = await axios
         .get(
           `https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails&forUsername=${channelUsername}&key=${process.env.API_KEY}`
         )
         .catch(err => console.log(err))
       console.log(channel)
+      // Fetching the separate videos in that playlist
       let recentUploadsPlaylistId =
         channel.data.items[0].contentDetails.relatedPlaylists.uploads
       let channelRecentUploads = await axios
@@ -43,6 +46,8 @@ export default {
         )
         .catch(err => console.log(err))
 
+      // Mapping the data to a simpler format containing 
+      // only the required fields
       let channelTitle = channelRecentUploads.data.items[0].snippet.channelTitle
       this.channelVideosData.push({
         [channelTitle]: channelRecentUploads.data.items.map(
